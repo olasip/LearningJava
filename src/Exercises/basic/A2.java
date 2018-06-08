d69d8d20b8c06574c5f67f0eb75e263c0705aabb
Aleksandra <aleksandrysip@gmail.com>
2018-06-08 16:24:50 +0200

    exersises

---
+++ src/Exercises/basic/A2.java
@@ -0,0 +1,24 @@
+package Exercises.basic;
+
+public class A2 {
+	public static void main(String[] args) {
+		
+		System.out.println("zad.1.");
+		int a = 0;
+		int b = 100;
+		int c = a * b + 5 % 2;
+		System.out.println(c);
+		
+		// initialize means to add first value to e.g variable
+		
+		System.out.println("\nzad.2.");
+		int x = 3;
+		int y = 5;
+		System.out.println(y % x * (++x) / 8);
+		
+		
+		
+	}
+	
+
+}
---
+++ src/Tutorial/AccessModifiers.java
@@ -0,0 +1,40 @@
+package Tutorial;
+
+public class AccessModifiers {
+	
+		
+	// private is the most restrictive 	
+	// default 
+	// protected	
+	// public
+		
+	private int x;
+	public int y;
+	
+	public static void main (String[] args) {
+		
+	}
+		
+		
+	
+	
+	// public visibility means that this method can be accessed anywhere in the program so long as you have an instance of this class to call it from
+	public void doSomethingPublic() {
+	}
+	
+	// private visibility means that method can't be accessed anywhere other than inside of this class
+	private void doSomethingPrivate() {
+    }
+	
+	// protected visibility means that this method can only be accessed inside of this method package, and from subclasses of this class
+	protected void doSomethingProtected() {	
+	}
+	
+	//default visibility means that this method can be accessed only inside of this package 
+	void doSomething() {
+	}
+	
+	
+	
+
+}
